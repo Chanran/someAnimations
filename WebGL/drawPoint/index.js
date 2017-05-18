dfd91a6f085ab06cf7958478ed4deb8ec5f1f8cf
blue <rong650751@163.com>
2017-05-19 00:43:42 +0800

    修改WebGL画点代码

--- WebGL/drawPoint/index.js
+++ WebGL/drawPoint/index.js
@@ -1,7 +1,9 @@
 var VSHADER_SOURCE = 
+  'attribute vec4 a_Position;\n' + 
+  'attribute float a_PointSize;\n' +
   'void main(){\n' +
-  '  gl_Position = vec4(0.0,0.0,0.0,1.0);\n' +
-  '  gl_PointSize = 10.0;\n' +
+  '  gl_Position = a_Position;\n' +
+  '  gl_PointSize = a_PointSize;\n' +
   '}\n';
 
 var FSHADER_SOURCE = 
@@ -23,7 +25,16 @@ function main(){
     return;
   }
 
-  gl.clearColor(0.0,0.0,0.0,1.0);
+  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+  var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+  if (a_Position < 0 || a_PointSize < 0) {
+    console.log('Failed to get the storage location of a_Position or a_PointSize');
+    return;
+  }
+  gl.vertexAttrib3f(a_Position, 0.5, 0.5, 0.0);
+  gl.vertexAttrib1f(a_PointSize, 30.0);
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS,0,1);
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
