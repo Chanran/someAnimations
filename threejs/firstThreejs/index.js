e379613aa342a8c50dddd68acf1608dd7613997d
blue <rong650751@163.com>
2017-06-04 23:57:18 +0800

    修正threejs第一个程序

--- threejs/firstThreejs/index.js
+++ threejs/firstThreejs/index.js
@@ -1,7 +1,7 @@
 // 创建3D场景
 var scene = new THREE.Scene();  
 // 创建照相机
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth, window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 // 创建渲染器
 var renderer = new THREE.WebGLRenderer(); 
 //设置渲染器的尺寸
@@ -10,9 +10,9 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // 几何图形
-var geometry = new THREE.BoxGeometry(1,1,1);
+var geometry = new THREE.BoxGeometry(1, 1, 1);
 // 创建网格材质并设置材质的颜色
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 // 创建立方体
 var cube = new THREE.Mesh(geometry, material);
 // 在场景中添加立方体
